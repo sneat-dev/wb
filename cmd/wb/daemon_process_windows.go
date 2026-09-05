8b273e838c054524bff85ce8822c54a88df8be02
Alexander Trakhimenok <alex@trakhimenok.com>
2026-09-05 22:48:10 +0100

    fix(daemon): supervise managed starts on macOS

--- cmd/wb/daemon_process_windows.go
+++ cmd/wb/daemon_process_windows.go
@@ -5,6 +5,8 @@ package main
 import (
 	"context"
 	"os"
+	"os/exec"
+	"path/filepath"
 
 	"golang.org/x/sys/windows"
 )
@@ -36,6 +38,24 @@ func stopDaemonProcess(pid int) error {
 		return err
 	}
 	return process.Kill()
+}
+
+func startDaemonProcess(executable string, args []string, logPath string) (int, error) {
+	if err := os.MkdirAll(filepath.Dir(logPath), 0o700); err != nil {
+		return 0, err
+	}
+	log, err := os.OpenFile(logPath, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0o600)
+	if err != nil {
+		return 0, err
+	}
+	command := exec.Command(executable, args...)
+	command.Stdout, command.Stderr, command.Stdin = log, log, nil
+	if err := command.Start(); err != nil {
+		_ = log.Close()
+		return 0, err
+	}
+	_ = log.Close()
+	return command.Process.Pid, nil
 }
 
 func signalDaemonContext(parent context.Context) (context.Context, context.CancelFunc) {
